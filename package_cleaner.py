2dffdc29194324e3bbde3b0baca27403c043182e
yalefresne <yann.lefresne@mylittleparis.com>
2024-01-23 23:38:43 +0100

    Feat: Deletion of untagged package version

--- package_cleaner.py
+++ package_cleaner.py
@@ -4,6 +4,8 @@ import os
 import sys
 import webbrowser
 
+from datetime import datetime
+from pyfiglet import Figlet
 from typing import Tuple
 
 # Project's module
@@ -15,20 +17,66 @@ from settings import Settings
 
 parser = argparse.ArgumentParser(
         prog='packclean',
-        description='Clean your packages Github'
+        description='Clean your packages Github.'
         )
 
 parser.add_argument(
         '-c',
         '--config',
-        help='Configure this tool interactively',
+        help='Configure this tool interactively.',
         action='store_true',
         )
 
-args = parser.parse_args()
+parser.add_argument(
+        '-ls',
+        '--list',
+        help='List organization\'s packages.',
+        action='store_true',
+        )
+
+parser.add_argument(
+        '-p',
+        '--package',
+        help='List package\'s versions.',
+        type=str,
+        )
+
+parser.add_argument(
+        '--untagged',
+        help='Remove untagged package\'s versions.',
+        type=str,
+        metavar='PACKAGE',
+        )
+
+parser.add_argument(
+        '--json',
+        help='Save result in a json file.',
+        action='store_true',
+        )
+
+parser.add_argument(
+        '-y',
+        '--yes',
+        help='Don\'t ask for confirmation.',
+        action='store_true',
+        )
+
+parser.add_argument(
+        '-a',
+        '--all',
+        help='Action as effect on all elements.',
+        action='store_true',
+        )
+
+parser.add_argument(
+        '--dry-run',
+        help='Action as no effect, just print what will be done.',
+        action='store_true',
+        )
 
 
-def configure() -> Tuple:
+def configure(all_mode: bool = False, dry_run_mode: bool = False) -> Tuple:
+    utils.beautyPrint('configuration')
     settings = Settings()
     github = Github()
 
@@ -41,7 +89,7 @@ def configure() -> Tuple:
             updateParam = getattr(github, f'update{snake_param}')
 
         if hasattr(settings, param.lower()) and getattr(settings, param.lower()):
-            if utils.yesNoQuestion(f'Do you want to update {param}'):
+            if all_mode or utils.yesNoQuestion(f'Do you want to update {param}'):
                 config[elem_param] = updateParam()
             else:
                 config[elem_param] = getattr(settings, param.lower())
@@ -54,19 +102,135 @@ def configure() -> Tuple:
     return config
 
 
-def run_command(command: str):
+def listPackage(save_mode: bool = False):
+    utils.beautyPrint('packages list')
+    with Github() as gh:
+        packages = gh.listPackages()
+
+    if save_mode:
+        utils.save_json('packages_list', packages)
+
+    for package in packages:
+        print(package['name'])
+
+
+def packageVersions(package: str, save_mode: bool = False) -> None:
+    utils.beautyPrint(f'{package} versions list')
+    with Github() as gh:
+        versions = gh.listVersions(package)
+    if save_mode:
+        utils.save_json(f'{package}_versions_list', versions)
+
+    for version in versions:
+        created_at = datetime.strptime(version['created_at'], '%Y-%m-%dT%H:%M:%SZ')
+        print(created_at,
+              version['html_url'],
+              version['metadata']['container']['tags']
+              )
+
+
+def deleteUntaggedVersion(
+        package: str,
+        version_id: str,
+        dry_run_mode: bool = False,
+        ) -> int:
+    with Github() as gh:
+        if not dry_run_mode:
+            response = gh.deleteVersion(package, version_id)
+
+    if dry_run_mode or response and response.status_code == 204:
+        print(f'Package version {version_id} deleted')
+        return 1
+    else:
+        print('Error during process')
+        print(response)
+        return 0
+
+
+def deleteUntaggedVersions(
+        package: str,
+        all_mode: bool = False,
+        dry_run_mode: bool = False,
+        save_mode: bool = False
+        ) -> None:
+    count_deleted = 0
+    count_total = 0
+
+    utils.beautyPrint(f'Delete untagged {package} versions')
+    with Github() as gh:
+        versions = gh.listVersions(package)
+
+        for version in versions:
+            created_at = datetime.strptime(version['created_at'], '%Y-%m-%dT%H:%M:%SZ')
+            tags = version['metadata']['container']['tags']
+            version_id = version['id']
+
+            if not tags:
+                count_total += 1
+                print(created_at, version['html_url'])
+
+                if save_mode:
+                    utils.save_json(f"{package}_{version_id}_untagged", version)
+
+                if all_mode:
+                    count_deleted += deleteUntaggedVersion(package, version_id, dry_run_mode)
+                    continue
+
+                user_answer = utils.yesNoWithOptionsQuestion(f'Confirm deletion version {version_id}')
+                if user_answer == utils.ANSWER_YES:
+                    count_deleted += deleteUntaggedVersion(package, version_id, dry_run_mode)
+                elif user_answer == utils.ANSWER_ALL:
+                    count_deleted += deleteUntaggedVersion(package, version_id, dry_run_mode)
+                    all_mode = True
+                elif user_answer == utils.ANSWER_QUIT:
+                    sys.exit(1)
+
+        print(f'Deleted versions: {count_deleted}/{count_total}')
+
+
+args = parser.parse_args()
+
+if 1 == len(sys.argv):
+    f = Figlet(font='slant')
+    print(f.renderText('packclean'))
+    parser.print_help()
+    parser.exit(0)
+
+dry_run_mode = True if args.dry_run else False
+not_confirm = True if args.yes or args.all else False
+save_mode = True if args.json else False
+
+if dry_run_mode:
+    print('Dry run mode: Action as no effect, just print what will be done.')
+if save_mode:
+    print('Save result in a json file.')
+try:
+    if args.config:
+        configure(
+                not_confirm,
+                dry_run_mode,
+                )
+    if args.list:
+        listPackage(
+                save_mode
+                )
+    if args.package:
+        packageVersions(
+                args.package,
+                save_mode
+                )
+    if args.untagged:
+        deleteUntaggedVersions(
+                args.untagged,
+                not_confirm,
+                dry_run_mode,
+                save_mode
+                )
+except KeyboardInterrupt:
+    print('\nInterrupted')
     try:
-        command()
-    except KeyboardInterrupt:
-        print('\nInterrupted')
-        try:
-            sys.exit(130)
-        except SystemExit:
-            os._exit(130)
-    sys.exit(1)
+        parser.exit(130)
+    except SystemExit:
+        os._exit(130)
 
-
-if args.config:
-    run_command(configure)
-
-parser.print_help()
+parser.exit(1)
--- utils.py
+++ utils.py
@@ -1,5 +1,12 @@
+import json
 import os
 import tempfile
+
+
+ANSWER_YES = 'yes'
+ANSWER_NO = 'no'
+ANSWER_ALL = 'all'
+ANSWER_QUIT = 'quit'
 
 
 def yesNoQuestion(question: str) -> bool:
@@ -10,6 +17,27 @@ def yesNoQuestion(question: str) -> bool:
         return True
 
     return False
+
+
+def yesNoWithOptionsQuestion(question: str) -> str:
+    yes_answer = ['yes', 'y', 'Yes', 'YES', 'Y`', '']
+    no_answer = ['no', 'n', 'No', 'NO', 'N']
+    all_answer = ['all', 'a', 'All', 'ALL', 'A']
+    quit_answer = ['quit', 'q', 'Quit', 'QUIT', 'Q']
+
+    answer = input(f'{question} ? y/n/q/a [yes]: ')
+    if answer in str(yes_answer):
+        return ANSWER_YES
+    if answer in str(no_answer):
+        return ANSWER_NO
+    if answer in str(all_answer):
+        return ANSWER_ALL
+    if answer in str(quit_answer):
+        return ANSWER_QUIT
+
+    ask_question_again_prefix = 'Bad format answer: '
+    question_clean = question.removeprefix(ask_question_again_prefix)
+    yesNoWithOptionsQuestion(f'Bad format answer: {question_clean}')
 
 
 def updateEnvFile(var: str, value: str):
@@ -44,3 +72,21 @@ def underscore_to_camelcase(value):
     """
     temp = value.split('_')
     return ''.join(elem.title() for elem in temp)
+
+
+def save_json(name: str, data) -> None:
+    json_folder = 'json'
+    # Serializing json
+    json_object = json.dumps(data, indent=4)
+
+    file = open(f'{json_folder}/{name}.json', 'w', encoding='utf8')
+    file.write(json_object)
+    file.close()
+
+
+def beautyPrint(desc: str, delimiter: str = '-', newline: bool = True):
+    desc = ' ' + desc + ' '
+    desc = desc.title().center(60, delimiter)
+    if newline:
+        desc = '\n' + desc + '\n'
+    print(desc)
